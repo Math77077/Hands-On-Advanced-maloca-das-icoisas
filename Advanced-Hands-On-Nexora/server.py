db5f42f2ad7cd3387a7dee8783670327fd2d915d
Vinicius Martins <147073086+vinimartinsufrr@users.noreply.github.com>
2025-04-18 17:23:16 -0400

    Update server.py

--- Advanced-Hands-On-Nexora/server.py
+++ Advanced-Hands-On-Nexora/server.py
@@ -7,22 +7,6 @@ knn = joblib.load("knn_jaundice_model.pkl")
 
 app = Flask(__name__)
 
-# Training data: [R, G, B, Label], where 1 = Jaundice
-dados_treinamento = np.array([
-    [0.9, 0.7, 0.5, 0],
-    [0.8, 0.6, 0.4, 0],
-    [0.7, 0.5, 0.3, 1],
-    [0.6, 0.4, 0.2, 1],
-    [0.85, 0.65, 0.45, 0],
-    [0.65, 0.45, 0.25, 1]
-])
-
-X_train = dados_treinamento[:, :-1]
-y_train = dados_treinamento[:, -1]
-
-knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_train, y_train)
-
 @app.route('/rgb', methods=['POST'])
 def classify_rgb():
     data = request.get_json()
@@ -30,7 +14,6 @@ def classify_rgb():
     green = data.get("green", 0)
     blue = data.get("blue", 0)
 
-    # Normalize values
     r_norm = red / 255.0
     g_norm = green / 255.0
     b_norm = blue / 255.0
